79055d68b160a611fa391fcca25c5be23922a26a
Gabriel Sales <ghe_sales@hotmail.com>
2023-03-27 16:22:11 -0300

    Refact messages

--- MauiSampleLogin/Contracts/CreateAccount/CreateAccountContract.cs
+++ MauiSampleLogin/Contracts/CreateAccount/CreateAccountContract.cs
@@ -1,4 +1,5 @@
 ﻿using Flunt.Validations;
+using MauiSampleLogin.Helper;
 using MauiSampleLogin.Models.CreateAccount;
 
 namespace MauiSampleLogin.Contracts.CreateAccount
@@ -8,14 +9,14 @@ namespace MauiSampleLogin.Contracts.CreateAccount
         public CreateAccountContract(CreateAccountRequest createAccount)
         {
             Requires()
-                .IsNotNullOrEmpty(createAccount.Name, nameof(createAccount.Name), "Name is invalid.");
+                .IsNotNullOrEmpty(createAccount.Name, nameof(createAccount.Name), Messages.NameIsInvalid);
 
             Requires()
-                .IsEmail(createAccount.Email, nameof(createAccount.Email), "E-mail is invalid.")
-                .IsNotNullOrEmpty(createAccount.Email, nameof(createAccount.Email), "E-mail is invalid.");
+                .IsEmail(createAccount.Email, nameof(createAccount.Email), Messages.EmailIsInvalid)
+                .IsNotNullOrEmpty(createAccount.Email, nameof(createAccount.Email), Messages.EmailIsInvalid);
 
             Requires()
-                .IsNotNullOrEmpty(createAccount.Password, nameof(createAccount.Password), "Password is invalid.");
+                .IsNotNullOrEmpty(createAccount.Password, nameof(createAccount.Password), Messages.PasswordIsInvalid);
         }
     }
 }
---
+++ MauiSampleLogin/Helper/Messages.cs
@@ -0,0 +1,14 @@
+﻿using System.Xml.Linq;
+
+namespace MauiSampleLogin.Helper
+{
+    public static class Messages
+    {
+        public const string NameIsInvalid = "Name is invalid.";
+
+        public const string EmailIsInvalid = "E-mail is invalid.";
+
+        public const string PasswordIsInvalid = "Password is invalid.";
+
+    }
+}

50ce4bf978d3424f57b92083bac260d125e85960
Kainã Fontoura Almeida <kainafa@hotmail.com>
2023-03-28 20:35:46 -0300

    Validação e integração da nossa viewmodel

--- MauiSampleLogin/ViewModels/MainViewModel.cs
+++ MauiSampleLogin/ViewModels/MainViewModel.cs
@@ -1,4 +1,6 @@
-﻿using MauiSampleLogin.Services;
+﻿using CommunityToolkit.Maui.Alerts;
+using MauiSampleLogin.Contracts.Login;
+using MauiSampleLogin.Services;
 
 namespace MauiSampleLogin.ViewModels;
 
@@ -20,15 +22,39 @@ public partial class MainViewModel
     [RelayCommand]
     public async Task Login()
     {
-        var result = await _loginService.LoginAsync(new Models.LoginRequest
+        if (Xamarin.Essentials.Connectivity.NetworkAccess == Xamarin.Essentials.NetworkAccess.Unknown ||
+            Xamarin.Essentials.Connectivity.NetworkAccess == Xamarin.Essentials.NetworkAccess.None)
+        {
+            await Toast.Make("Sem conexão com a internet", CommunityToolkit.Maui.Core.ToastDuration.Long).Show();
+            return;
+        }
+
+        var loginRequest = new Models.LoginRequest
         {
             Email = email,
             Password = password
-        });
+        };
 
-        if (!string.IsNullOrEmpty(result.Token))
+        var validator = new LoginContract(loginRequest);
+        if (!validator.IsValid)
         {
+            await Toast.Make("Dados são inválidos", CommunityToolkit.Maui.Core.ToastDuration.Long).Show();
+            return;
         }
+
+        var result = await _loginService.LoginAsync(loginRequest);
+
+        if (string.IsNullOrEmpty(result.Access_Token))
+        {
+            await Toast.Make("A requisação falhou, tente novamente.", CommunityToolkit.Maui.Core.ToastDuration.Long).Show();
+            return;
+        }
+
+        await SecureStorage.SetAsync("token", result.Access_Token);
+        await SecureStorage.SetAsync("refreshToken", result.Refresh_Token);
+
+        await Shell.Current.GoToAsync($"//{nameof(ProductsPage)}");
+
     }
 
     [RelayCommand]
